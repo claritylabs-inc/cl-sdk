bbcb6dfc7bc761199c6893200dea05c9bf28d639
Terry Wang <wang.terry375@gmail.com>
2026-08-05 00:17:50 -0500

    chore: remove deprecated package surfaces (#7)

--- src/prompts/query/index.ts
+++
@@ -1,5 +0,0 @@
-export { buildQueryClassifyPrompt } from "./classify";
-export { buildInterpretAttachmentPrompt } from "./interpret-attachment";
-export { buildReasonPrompt } from "./reason";
-export { buildVerifyPrompt } from "./verify";
-export { buildRespondPrompt } from "./respond";
--- src/query/index.ts
+++
@@ -1,2 +0,0 @@
-export { createQueryAgent } from "./coordinator";
-export type { QueryConfig, QueryInput, QueryOutput } from "./types";
